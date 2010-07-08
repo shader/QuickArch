3bd06c1dcfb06216d51b34efdfd6e39829cafeb2
Scott <industryunleash@gmail.com>
2010-07-08 14:35:57 -0400

    templated the ItemsControl ItemPanelTemplate so it displays children in a canvas

--- View/ComponentView.xaml.cs
+++ View/ComponentView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace QuickArch.View
 {
