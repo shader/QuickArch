8f4f505005a9e78844921b7662886bd48a1d7b7c
Scott <industryunleash@gmail.com>
2010-07-08 12:55:38 -0400

    Created ConnectorViewModel.cs class

---
+++ ViewModel/ConnectorViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickArch.Model;
+using QuickArch.DataAccess;
+
+namespace QuickArch.ViewModel
+{
+    public class ConnectorViewModel : WorkspaceViewModel
+    {
+        readonly Connector connector;
+        readonly ComponentManager componentManager;
+        bool isSelected;
+
+        #region Constructor
+        public ConnectorViewModel(Connector connector, ComponentManager componentManager)
+        {
+            if (connector == null)
+                throw new ArgumentNullException("connector");
+            if (componentManager == null)
+                throw new ArgumentNullException("componentManager");
+
+            this.connector = connector;
+            this.componentManager = componentManager;
+        }
+        #endregion
+
+        #region Connector Properties
+
+        public Component Start
+        {
+            get { return connector.Start; }
+            set
+            {
+                if (value == connector.Start)
+                    return;
+
+                connector.Start = value;
+
+                base.OnPropertyChanged("Start");
+            }
+        }
+
+        public Component End
+        {
+            get { return connector.End; }
+            set
+            {
+                if (value == connector.End)
+                    return;
+
+                connector.End = value;
+
+                base.OnPropertyChanged("End");
+            }
+        }
+
+        #endregion
+
+        #region Presentation Properties
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                if (value == isSelected)
+                    return;
+
+                isSelected = value;
+
+                base.OnPropertyChanged("IsSelected");
+            }
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        #endregion
+    }
+}
