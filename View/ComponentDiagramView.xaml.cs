314e81c578c22d1d19f8da2d673ef68e816ea28e
unknown <Scott@.(none)>
2010-07-14 09:58:35 -0400

    trying to get ResizingAdorner.cs to work in myCanvas

--- View/ComponentDiagramView.xaml.cs
+++ View/ComponentDiagramView.xaml.cs
@@ -34,13 +34,107 @@ namespace QuickArch.View
         {
             InitializeComponent();
         }
-        /*
+        
         private void Diagram_Created(object sender, RoutedEventArgs e)
         {
             this.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(Diagram_MouseLeftButtonDown);
             this.PreviewMouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
             this.PreviewMouseMove += new MouseEventHandler(Diagram_MouseMove);
+            this.MouseLeave += new MouseEventHandler(Diagram_MouseLeave);
+
+            myCanvas.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(myCanvas_PreviewMouseLeftButtonDown);
+            myCanvas.PreviewMouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
         }
-         */
+
+        // Handler for drag stopping on leaving the window
+        void Diagram_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StopDragging();
+            e.Handled = true;
+        }
+
+        // Handler for drag stopping on user choise
+        void DragFinishedMouseHandler(object sender, MouseButtonEventArgs e)
+        {
+            StopDragging();
+            e.Handled = true;
+        }
+
+        // Method for stopping dragging
+        private void StopDragging()
+        {
+            if (_isDown)
+            {
+                _isDown = false;
+                _isDragging = false;
+            }
+        }
+
+        // Hanler for providing drag operation with selected element
+        void Diagram_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDown)
+            {
+                if ((_isDragging == false) &&
+                    ((Math.Abs(e.GetPosition(myCanvas).X - _startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
+                    (Math.Abs(e.GetPosition(myCanvas).Y - _startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
+                    _isDragging = true;
+
+                if (_isDragging)
+                {
+                    Point position = Mouse.GetPosition(myCanvas);
+                    Canvas.SetTop(selectedElement, position.Y - (_startPoint.Y - _originalTop));
+                    Canvas.SetLeft(selectedElement, position.X - (_startPoint.X - _originalLeft));
+                }
+            }
+        }
+
+        // Handler for clearing element selection, adorner removal
+        void Diagram_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selected)
+            {
+                selected = false;
+                if (selectedElement != null)
+                {
+                    aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                    selectedElement = null;
+                }
+            }
+        }
+
+        // Handler for element selection on the canvas providing resizing adorner
+        void myCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Remove selection on clicking anywhere the window
+            if (selected)
+            {
+                selected = false;
+                if (selectedElement != null)
+                {
+                    // Remove the adorner from the selected element
+                    aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                    selectedElement = null;
+                }
+            }
+
+            // If any element except canvas is clicked, 
+            // assign the selected element and add the adorner
+            if (e.Source != myCanvas)
+            {
+                _isDown = true;
+                _startPoint = e.GetPosition(myCanvas);
+
+                selectedElement = e.Source as UIElement;
+
+                _originalLeft = Canvas.GetLeft(selectedElement);
+                _originalTop = Canvas.GetTop(selectedElement);
+
+                aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
+                aLayer.Add(new ResizingAdorner(selectedElement));
+                selected = true;
+                e.Handled = true;
+            }
+        }
     }
 }
