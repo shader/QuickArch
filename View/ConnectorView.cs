eab692978df7138cedc5df7a2c77c542a765ec6d
unknown <Scott@.(none)>
2010-07-20 10:12:23 -0400

    ConnectorView.cs

---
+++ View/ConnectorView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows;
+
+namespace QuickArch.View
+{
+    public class ConnectorView : Shape
+    {
+        LineGeometry linegeo;
+
+        public static readonly DependencyProperty StartPointProperty = DependencyProperty.Register("StartPoint", typeof(Point), typeof(ConnectorView), new FrameworkPropertyMetadata(new Point(0, 0), FrameworkPropertyMetadataOptions.AffectsMeasure));
+        public static readonly DependencyProperty EndPointProperty = DependencyProperty.Register("EndPoint", typeof(Point), typeof(ConnectorView), new FrameworkPropertyMetadata(new Point(0, 0), FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public Point StartPoint
+        {
+            get { return (Point)GetValue(StartPointProperty); }
+            set { SetValue(StartPointProperty, value); }
+        }
+
+        public Point EndPoint
+        {
+            get { return (Point)GetValue(EndPointProperty); }
+            set { SetValue(EndPointProperty, value); }
+        }
+
+        public ConnectorView()
+        {
+            linegeo = new LineGeometry();
+                        
+            this.Stroke = Brushes.Black;
+            this.StrokeThickness = 2;
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                linegeo.StartPoint = StartPoint;
+                linegeo.EndPoint = EndPoint;
+                return linegeo;
+            }
+        }
+    }
+}
---
+++ View/MyThumb.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace QuickArch.View
+{
+    public class MyThumb : Thumb
+    {
+        public Guid ID
+        {
+            get { return id; }
+        }
+        public Point Position
+        {
+            get { return position; }
+        }
+
+        #region Class Variables
+        public List<LineGeometry> EndLines { get; private set; }
+        public List<LineGeometry> StartLines { get; private set; }
+        private Guid id;
+        private Point position;
+        #endregion
+
+        #region Constructors
+        public MyThumb() : base()
+        {
+            StartLines = new List<LineGeometry>();
+            EndLines = new List<LineGeometry>();
+            id = Guid.NewGuid();
+            position = new Point();
+        }
+
+        public MyThumb(ControlTemplate template, Point position) : this()
+        {
+            this.Template = template;
+            this.SetPosition(position);
+        }
+
+        public MyThumb(ControlTemplate template, Point position, DragDeltaEventHandler dragDelta) : this(template, position)
+        {
+            this.DragDelta += dragDelta;
+        }
+        #endregion
+
+        // Helper method for setting the position of our thumb
+        public void SetPosition(Point value)
+        {
+            Canvas.SetLeft(this, value.X);
+            Canvas.SetTop(this, value.Y);
+        }
+
+        #region Linking logic
+        // This method establishes a link between current thumb and specified thumb.
+        // Returns a line geometry with updated positions to be processed outside.
+        public LineGeometry LinkTo(MyThumb target)
+        {
+            // Create new line geometry
+            LineGeometry line = new LineGeometry();
+            // Save as starting line for current thumb
+            this.StartLines.Add(line);
+            // Save as ending line for target thumb
+            target.EndLines.Add(line);
+            // Ensure both thumbs the latest layout
+            this.UpdateLayout();
+            target.UpdateLayout();
+            // Update line position
+            line.StartPoint = new Point(Canvas.GetLeft(this) + this.ActualWidth / 2, Canvas.GetTop(this) + this.ActualHeight / 2);
+            line.EndPoint = new Point(Canvas.GetLeft(target) + target.ActualWidth / 2, Canvas.GetTop(target) + target.ActualHeight / 2);
+            // return line for further processing
+            return line;
+        }
+
+        // This method establishes a link between current thumb and target thumb using a predefined line geometry
+        // Note: this is commonly to be used for drawing links with mouse when the line object is predefined outside this class
+        public bool LinkTo(MyThumb target, LineGeometry line)
+        {
+            // Save as starting line for current thumb
+            this.StartLines.Add(line);
+            // Save as ending line for target thumb
+            target.EndLines.Add(line);
+            // Ensure both tumbs the latest layout
+            this.UpdateLayout();
+            target.UpdateLayout();
+            // Update line position
+            line.StartPoint = new Point(Canvas.GetLeft(this) + this.ActualWidth / 2, Canvas.GetTop(this) + this.ActualHeight / 2);
+            line.EndPoint = new Point(Canvas.GetLeft(target) + target.ActualWidth / 2, Canvas.GetTop(target) + target.ActualHeight / 2);
+            return true;
+        }
+        #endregion
+
+        // This method updates all the starting and ending lines assigned for the given thumb 
+        // according to the latest known thumb position on the canvas
+        public void UpdateLinks()
+        {
+            double left = Canvas.GetLeft(this);
+            double top = Canvas.GetTop(this);
+
+            for (int i = 0; i < this.StartLines.Count; i++)
+                this.StartLines[i].StartPoint = new Point(left + this.ActualWidth / 2, top + this.ActualHeight / 2);
+
+            for (int i = 0; i < this.EndLines.Count; i++)
+                this.EndLines[i].EndPoint = new Point(left + this.ActualWidth / 2, top + this.ActualHeight / 2);
+        }
+        private void onDragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            // Exit dragging operation during adding new link
+            //if (isAddNewLink) return;
+
+            MyThumb thumb = e.Source as MyThumb;
+
+            Canvas.SetLeft(thumb, Canvas.GetLeft(thumb) + e.HorizontalChange);
+            Canvas.SetTop(thumb, Canvas.GetTop(thumb) + e.VerticalChange);
+
+            // Update links' layouts for active thumb
+            //thumb.UpdateLinks();
+        }
+    }
+}
